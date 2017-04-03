2a7a21f345b6bf983eb0b8d7d5f04503e1857d8c
bdot868 <brandoncarter100@gmail.com>
2017-04-03 09:52:55 -0700

    added a readme file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var destinationBox = document.querySelectorAll('.circle')
+var destinationBox = $('.circle')
 var start = $('#start')
 var stop = $('#stop')
 var red = $('#red')
 var black = $('#black')
-var currentPlayer;
+var currentPlayer = null;
 var players = {
   red: '<img src="red piece.jpg" alt="">',
   black: '<img src="black piece.jpg">'
@@ -14,52 +14,76 @@ var players = {
 /*I want to allow the first user to pick their color of choice & and make the
 2nd color default to the 2nd color*/
 start.on('click', gameStart)
+stop.on('click', gameQuit)
 
 function gameStart(){
-  alert('Player please select your color');
+  alert('Player 1 please select your color');
   red.on('click', function(){
     currentPlayer = players.red
   })
   black.on('click', function(){
     currentPlayer = players.black
   })
+  gamePlay()
+}
+
+function gameQuit() {
+  if(window.confirm('Are you sure you want to quit?')){
+    gameDefault()
+  }
+}
+
+function gameDefault(){
+  for (var i = 0; i < destinationBox.length; i++) {
+    destinationBox[i].innerHTML = ''
+    currentPlayer = null
+  }
 
 }
+
 //alert(currentPlayer + ' make your move!')
 
 //var currentPlayer = userPick;
 /* upon starting the game I want to randomize who gets to go first,
    and prompt them to make a move*/
-function switchTurns(){
-  if(currentPlayer == players.red){
-    currentPlayer = players.black;
-  } else {
-    currentPlayer = players.red;
-  }
-}
+
 
 
    /* I will create jquery functions to move the pieces across the top of the
    board and then drop down into the respective div that was clicked by each
    player */
-   for (var i = 0; i < destinationBox.length; i++) {
-     destinationBox[i].addEventListener('click', function(){
-       this.innerHTML = currentPlayer
+
+   function gamePlay(){
+     destinationBox.on('click', function(){
+       $(this).innerHTML = currentPlayer
      })
-     destinationBox[i].addEventListener('click', winnerIs)
-   }
+     destinationBox.on('click', winnerIs)
+    //  for (var i = 0; i < destinationBox.length; i++) {
+    //    destinationBox[i].addEventListener('click', function(){
+    //      this.innerHTML = currentPlayer
+      //  })
+      //  destinationBox[i].addEventListener('click', winnerIs)
+     }
+   //}
 
    //I will create functions to toggle between the players to alternate turns
+   function switchTurns(){
+     if(currentPlayer == players.red){
+       currentPlayer = players.black;
+     } else {
+       currentPlayer = players.red;
+     }
+   }
 
    //I will create a function that will check every time a piece is placed if
    //there is a winner by seeing if anybody has four in a row
    function winnerIs(){
      if(destinationBox[0].innerHTML !== ''){
        if(destinationBox[0].innerHTML == destinationBox[1].innerHTML || destinationBox[0].innerHTML == destinationBox[2].innerHTML){
-         console.log('You won!')
-         switchTurns()
+         console.log( currentPlayer + 'You won!')
        }
      }
+     switchTurns()
    }
 
    //Create logic for displaying who won on the screen
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Connect Four
+### pound sign is h1 - h6
+
+an example of inline-code:
+Run `nodemon` from the terminal
+
+an example of code block:
+
+
+```javascript
+function helloWorld(){
+	console.log()
+	}
+```
+
+---
+separator is ---
+
+---
+Images:
+
+![This is a kitten](documentation/kitty.gif)
+
+[]() - a clickable link
+
+![]() - an img
