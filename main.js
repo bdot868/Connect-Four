e754c6602c40542885af96cb70f43a086e49bdbb
bdot868 <brandoncarter100@gmail.com>
2017-04-03 19:20:54 -0700

    got winner logic working fully

--- main.js
+++ main.js
@@ -4,15 +4,11 @@ var stop = $('#stop')
 var red = $('#red')
 var black = $('#black')
 var currentPlayer = null;
-var players = {
-  red: '<img src="red piece.jpg" alt="">',
-  black: '<img src="black piece.jpg">'
-}
-
-var horizontal = [[0,1,2,3,4,5,6], [7,8,9,10,11,12,13], [14,15,16,17,18,19,20], [21,22,23,24,25,26,27], [28,29,30,31,32,33,34], [35,36,37,38,39,40,41]]
-var vertical = [[destinationBox[1]], [], [], [], [], [], []]
-var diagonal = [[], [], [], [], [], [],
-                [], [], [], [], [], []]
+var pieces = [
+  {marker: '<img src="red piece.jpg" alt="">', color: 'Red'},
+  {marker: '<img src="black piece.jpg">', color: 'Black'}
+]
+var player1, player2;
 
 gameDefault()
 /*I want to allow the first user to pick their color of choice & and make the
@@ -23,10 +19,14 @@ stop.on('click', gameQuit)
 function gameStart(){
   alert('Player 1 please select your color');
   red.on('click', function(){
-    currentPlayer = players.red
+    currentPlayer = pieces[0]
+    //player1 = pieces[0].color
+    //player2 = pieces[1].color
   })
   black.on('click', function(){
-    currentPlayer = players.black
+    currentPlayer = pieces[1]
+    //player1 = pieces[1].color
+    //player2 = pieces[0].color
   })
   gamePlay()
 }
@@ -61,8 +61,8 @@ function gameDefault(){
 
    function gamePlay(){
      destinationBox.on('click', function(){
-       $(this).html(currentPlayer)
-       winnerIs()
+       $(this).html(currentPlayer.marker)
+       checkWinner()
      })
     //  destinationBox.on('click', winnerIs)
 
@@ -77,41 +77,41 @@ function gameDefault(){
 
    //I will create functions to toggle between the players to alternate turns
    function switchTurns(){
-     if(currentPlayer == players.red){
-       currentPlayer = players.black;
+     if(currentPlayer == pieces[0]){
+       currentPlayer = pieces[1];
      } else {
-       currentPlayer = players.red;
+       currentPlayer = pieces[0];
      }
    }
 
    //I will create a function that will check every time a piece is placed if
    //there is a winner by seeing if anybody has four in a row
-   function winnerIs(){
+   function checkWinner(){
      for (var i = 0; i < destinationBox.length; i++) {
           if(destinationBox[i].innerHTML !== ''){
             if(i >= 0 && i <= 20){
-                 if((destinationBox[i].innerHTML == currentPlayer && destinationBox[i+7].innerHTML == currentPlayer && destinationBox[i+14].innerHTML == currentPlayer && destinationBox[i+21].innerHTML == currentPlayer) ||
-                    (destinationBox[i].innerHTML == currentPlayer && destinationBox[i+8].innerHTML == currentPlayer && destinationBox[i+16].innerHTML == currentPlayer && destinationBox[i+24].innerHTML == currentPlayer) ||
-                    (destinationBox[i].innerHTML == currentPlayer && destinationBox[i+6].innerHTML == currentPlayer && destinationBox[i+12].innerHTML == currentPlayer && destinationBox[i+18].innerHTML == currentPlayer)){
-                      console.log('it had to come to this - diagonal or vertical')
+                 if((destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i+7].innerHTML == currentPlayer.marker && destinationBox[i+14].innerHTML == currentPlayer.marker && destinationBox[i+21].innerHTML == currentPlayer.marker) ||
+                    (destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i+8].innerHTML == currentPlayer.marker && destinationBox[i+16].innerHTML == currentPlayer.marker && destinationBox[i+24].innerHTML == currentPlayer.marker) ||
+                    (destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i+6].innerHTML == currentPlayer.marker && destinationBox[i+12].innerHTML == currentPlayer.marker && destinationBox[i+18].innerHTML == currentPlayer.marker)){
+                      console.log('it had to come to this ' + currentPlayer.color + 'wins diagonal or vertical')
 
                    }
                  } else if(i >= 2 & i <= 40){
-                     if(destinationBox[i].innerHTML == currentPlayer && destinationBox[i-1].innerHTML == currentPlayer && destinationBox[i-2].innerHTML == currentPlayer && destinationBox[i+1].innerHTML == currentPlayer){
-                       console.log('I won horizontal')
+                     if(destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i-1].innerHTML == currentPlayer.marker && destinationBox[i-2].innerHTML == currentPlayer.marker && destinationBox[i+1].innerHTML == currentPlayer.marker){
+                       console.log(currentPlayer.color + ' won horizontal')
                      }
                  } else if(i >= 1 && i <= 39){
-                     if(destinationBox[i].innerHTML == currentPlayer && destinationBox[i+1].innerHTML == currentPlayer && destinationBox[i+2].innerHTML == currentPlayer && destinationBox[i-1].innerHTML == currentPlayer){
-                       console.log('I won horizontal')
+                     if(destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i+1].innerHTML == currentPlayer.marker && destinationBox[i+2].innerHTML == currentPlayer.marker && destinationBox[i-1].innerHTML == currentPlayer.marker){
+                       console.log(currentPlayer.color + ' won horizontal')
                      }
                  } else if(i <= 38 && i >= 0){
-                     if(destinationBox[i].innerHTML == currentPlayer && destinationBox[i+1].innerHTML == currentPlayer && destinationBox[i+2].innerHTML == currentPlayer && destinationBox[i+3].innerHTML == currentPlayer){
-                       console.log('I won hun')
+                     if(destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i+1].innerHTML == currentPlayer.marker && destinationBox[i+2].innerHTML == currentPlayer.marker && destinationBox[i+3].innerHTML == currentPlayer.marker){
+                       console.log(currentPlayer.color + ' won hun')
                      }
                  } else if(i >= 3){
-              if(destinationBox[i].innerHTML == currentPlayer && destinationBox[i-1].innerHTML == currentPlayer && destinationBox[i-2].innerHTML == currentPlayer && destinationBox[i-3].innerHTML == currentPlayer){
-                  console.log('winning is what i do!')
-                  
+              if(destinationBox[i].innerHTML == currentPlayer.marker && destinationBox[i-1].innerHTML == currentPlayer.marker && destinationBox[i-2].innerHTML == currentPlayer.marker && destinationBox[i-3].innerHTML == currentPlayer.marker){
+                  console.log('winning is ' + currentPlayer.color)
+
                 }
               }
             }
